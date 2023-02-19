f766fa809af432a689544a6019344d58dc910307
induwara <ravinduinduwara@gmail.com>
2023-02-19 17:35:27 +0530

    modified contentview

--- BasicBMI/ContentView.swift
+++ BasicBMI/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var weightText:String = ""
+    @State private var heightText:String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("BMI Calculator :").font(.largeTitle)
+            TextField("Enter Weight (in kilograms)",text: $weightText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .border(Color.black)
+            TextField("Enter Height (in kilograms)",text: $heightText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .border(Color.black)
         }
         .padding()
     }

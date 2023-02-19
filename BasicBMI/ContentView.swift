de24eed622f67fa7f6c96827e00392983deb5bc0
induwara <ravinduinduwara@gmail.com>
2023-02-19 20:26:27 +0530

    adding button to calculate BMI

--- BasicBMI/ContentView.swift
+++ BasicBMI/ContentView.swift
@@ -10,16 +10,34 @@ import SwiftUI
 struct ContentView: View {
     @State private var weightText:String = ""
     @State private var heightText:String = ""
+    @State private var bmi: Double = 0
     var body: some View {
         VStack {
             Text("BMI Calculator :").font(.largeTitle)
             TextField("Enter Weight (in kilograms)",text: $weightText)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .border(Color.black)
-            TextField("Enter Height (in kilograms)",text: $heightText)
+                .onChange(of: weightText) { value in
+                    print("weight changed")
+                }
+            TextField("Enter Height (in meters)",text: $heightText)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .border(Color.black)
-        }
+            Button {
+                let weight = Double(self.weightText)!
+                let height = Double(self.heightText)!
+                self.bmi = weight/(height * height)
+                }
+                label:{
+                    Text("Calculate BMI")
+                        .padding()
+                        .foregroundColor(Color(.white))
+                        .background(Color.blue)
+                    }
+            Text("BMI: \(bmi, specifier: "%.1f")")
+                        .font(.title)
+                        .padding()
+                }
         .padding()
     }
 }

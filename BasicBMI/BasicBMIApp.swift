bb59b0bff48e6a571559f2f32d5bb19e5eef2fa0
induwara <ravinduinduwara@gmail.com>
2023-02-19 12:52:08 +0530

    Initial Commit

---
+++ BasicBMI/BasicBMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicBMIApp.swift
+//  BasicBMI
+//
+//  Created by Ravindu Hettiarachchi on 2023-02-19.
+//
+
+import SwiftUI
+
+@main
+struct BasicBMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
